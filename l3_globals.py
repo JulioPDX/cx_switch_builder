51fc50c4e0093371f8bb0ffbf0688f31f2de9af5
lmccormick1922 <39968527+lmccormick1922@users.noreply.github.com>
2021-05-11 13:43:17 -0500

    Add files via upload

---
+++ l3_globals.py
@@ -0,0 +1,42 @@
+#!/root/anaconda3/bin/python
+#import modules
+import csv
+import os.path
+import os
+import time
+import csv
+import sys
+
+os.system('rm -rf *.txt')
+os.system('rm -rf ./output')
+os.system('rm -rf ./vrf_parts_output')
+os.system('mkdir ./output')
+os.system('mkdir ./vrf_parts_output')
+l3inventory_file = open('layer3_inventory.csv')
+l3globals_file = open('layer3_globals.csv')
+reader = csv.DictReader(l3globals_file)
+inventory = csv.DictReader(l3inventory_file)
+
+for row in reader:
+  switch = (row['Switch'])
+  vrf = (row['vrf'])
+  vrf_rd = (row['vrf_rd'])
+  ospf_id = (row['ospf_id'])
+  loopback_int = (row['loopback_int'])
+  loopback_addr = (row['loopback_addr'])
+  
+  if vrf != "global":
+    string = "vrf " + vrf + "\n" "rd " + vrf_rd + "\nexit\n\n" + "router ospf " + ospf_id + " vrf " + vrf + "\n" " router-id " + loopback_addr + "\n" + " area 0\n enable\n exit\n\n"
+    outfile = ("./vrf_parts_output/" + switch + "_" + vrf + ".txt")
+    write_out = open(outfile,"w")
+    write_out.write(string)
+
+  if vrf == "global":
+    string = "router ospf " + ospf_id + "\n" " router-id " + loopback_addr + "\n" + " area 0\n enable\n exit\n\n"
+    outfile = ("./vrf_parts_output/" + switch + "_global.txt")
+    write_out = open(outfile,"w")
+    write_out.write(string)
+
+for row in inventory:
+    switch = (row['Switch'])
+    os.system('cat ./vrf_parts_output/' + switch + '* >> ./output/' + switch + '_full.txt')
